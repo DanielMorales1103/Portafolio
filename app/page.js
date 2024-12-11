f32ef5fc23e290d69afda36929d0c47d915343c6
DanielMorales1103 <demoralesu@morurgt.com>
2024-12-11 13:31:45 -0600

    actualización 11/12/2024

--- app/page.js
+++ app/page.js
@@ -5,11 +5,7 @@ import {SiGmail} from "react-icons/si";
 import {IoLogoWhatsapp} from "react-icons/io";
 import Image from 'next/image';
 import devdm from "../public/dev-dm-wave.png";
-import web1 from "../public/web1.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
 import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
 import React, { useState } from 'react';
 
 export default function Home() {
@@ -18,7 +14,7 @@ export default function Home() {
   return (
     <div className={darkmode ? 'dark' : ''}>    
       <main className="bg-white px-4 sm:px-8 md:px-12 lg:px-20 xl:px-40 dark:bg-gray-900">
-        <section className="min-h-screen">
+        <section>
           <nav className="py-6 flex justify-between items-center">
             <h1 className='text-lg sm:text-xl font-burtons dark:text-gray-50'>developedbydm</h1>
             <ul className='flex items-center gap-4'>
@@ -33,81 +29,89 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className='text-center p-6'>
+          <div className='text-center mt-12'>
             <h2 className='text-4xl sm:text-5xl py-2 text-teal-600 font-medium md:text-6xl'>
               Daniel Morales</h2>
             <h3 className='text-lg sm:text-2xl py-2 md:text-3xl dark:text-gray-500'>Desarrollador</h3>
-            <p className='text-sm sm:text-md py-5 leading-6 sm:leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-gray-100'> 
-              Estudiante de quinto año de la carrera <span className='italic'>Ciencias de la Computación y Tecnologías de la Información</span>
-              en la Universidad del Valle de Guatemala.
+            <p className='text-sm sm:text-md py-5 leading-6 sm:leading-8 text-gray-800 md:text-xl mx-auto dark:text-gray-100 mb-6'> 
+              Soy estudiante de quinto año de <span className='italic'>Ciencias de la Computación y Tecnologías de la Información</span> con
+              interés en el desarrollo web, Inteligencia Artificial, Bases de Datos y Desarrollo de Software. Me
+              apasionan los retos y disfruto trabajar en equipo, donde puedo aportar mis ideas y aprender de los
+              demás. Soy responsable y me comprometo a aprender continuamente sobre las tecnologías y
+              tendencias que me apasionan, buscando siempre mejorar mis habilidades y contribuir de manera
+              significativa a los proyectos en los que participo.
             </p>
           </div>
           <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-60 sm:w-72 md:w-80 overflow-hidden'>
             <Image  src={devdm} />
           </div>
         </section>
+
+        <section className="flex flex-col md:flex-row gap-8 mt-8">
+          {/* Sección de Educación */}
+          <div className="md:w-1/2">
+            <h3 className="text-2xl sm:text-3xl py-1 md:py-4 dark:text-gray-500 text-center">EDUCACIÓN</h3>
+            <p className="text-sm sm:text-md py-2 leading-6 sm:leading-8 text-gray-800 md:text-xl dark:text-gray-100 text-left">
+              <strong>2021 - Actualidad:</strong> Universidad del Valle de Guatemala - Ingeniería en Ciencias de la Computación y Tecnologías de la Información
+            </p>
+            <p className="text-sm sm:text-md py-2 leading-6 sm:leading-8 text-gray-800 md:text-xl dark:text-gray-100 text-left">
+              <strong>2007 - 2020:</strong> Colegio Suizo Americano - Bachiller en Ciencias y Letras con orientación en Computación
+            </p>
+          </div>
+
+          {/* Sección de Habilidades */}
+          <div className="md:w-1/2">
+            <h3 className="text-2xl sm:text-3xl py-1 md:py-4 dark:text-gray-500 text-center">HABILIDADES</h3>
+            <ul className="text-sm sm:text-md py-2 leading-6 sm:leading-8 text-gray-800 md:text-xl dark:text-gray-100 list-disc list-inside text-left">
+              <li><strong>Lenguajes de Programación:</strong> Python, Java, JavaScript, PHP</li>
+              <li><strong>Desarrollo Web:</strong> HTML, CSS, Tailwind CSS, Bootstrap, React, Next.js</li>
+              <li><strong>Frameworks y Plataformas:</strong> Spring Boot, Django</li>
+              <li><strong>Bases de Datos:</strong> MySQL, MongoDB</li>
+              <li><strong>Herramientas y Control de Versiones:</strong> Docker, Git</li>
+              <li><strong>Idiomas:</strong> Español (Nativo), Inglés (B2)</li>
+            </ul>
+          </div>
+        </section>
+
+
+        <section className="mt-12">
+          <div className="md:w-3/4 mx-auto">
+            <h3 className="text-2xl sm:text-3xl py-1 md:py-4 dark:text-gray-500 text-center">EXPERIENCIA LABORAL</h3>
+            <div className="text-sm sm:text-md py-2 leading-6 sm:leading-8 text-gray-800 md:text-xl dark:text-gray-100 text-left">
+              <strong>Junior Developer</strong> <br />
+              <em>Spring-bus, Mar 2024 - Presente</em>
+              <ul className="list-disc list-inside">
+                <li>Desarrollé la aplicación web para la venta de boletos utilizando Next.js y Tailwind CSS en el front-end y Java con Spring Boot en el back-end.</li>
+                <li>Colaboré en la implementación de servicios y endpoints para la integración de datos en la plataforma.</li>
+                <li>Trabajé en el portal de backoffice para proveedores, usando Python, HTML, Bootstrap y Django.</li>
+                <li>Implementé consultas y lógica de acceso a datos en MySQL desde el backend.</li>
+              </ul>
+            </div>
+            <div className="text-sm sm:text-md py-2 leading-6 sm:leading-8 text-gray-800 md:text-xl dark:text-gray-100 text-left mt-6">
+              <strong>Junior Developer Intern</strong> <br />
+              <em>Pacifiko.com, Jun 2023 - Feb 2024</em>
+              <ul className="list-disc list-inside">
+                <li>Mejoré la página web de la tienda y el sistema interno utilizando PHP, JavaScript, HTML y CSS.</li>
+                <li>Automaticé tareas operativas con scripts en Python, reduciendo significativamente el tiempo de trabajo.</li>
+                <li>Realicé consultas y tareas de administración en la base de datos MySQL.</li>
+              </ul>
+            </div>
+          </div>
+        </section>
         
         <section>
           <div className='text-center'>
-            <h3 className='text-2xl sm:text-3xl py-1 md:py-4 mt-8 dark:text-gray-500'>Conocimientos</h3>
-            <p className='text-sm sm:text-md py-2 leading-6 sm:leading-8 text-gray-800 md:text-xl max-w-3xl mx-auto dark:text-gray-100'> 
-              Me gusta el mundo del Frontend. Me gusta diseñar interfaces de usuario atractivas y funcionales.
-              Durante mi aprendizaje he adquirido conocimientos en tecnologías y herramientas como 
-              <span className='text-teal-500'> HTML</span>, 
-              <span className='text-teal-500'> CSS</span>,
-              <span className='text-teal-500'> JavaScript</span>,
-              <span className='text-teal-500'> React</span> y 
-              <span className='text-teal-500'> KaboomJS</span>. 
-            </p>
-            <p className='text-sm sm:text-md py-2 leading-6 sm:leading-8 text-gray-800 md:text-xl max-w-3xl mx-auto dark:text-gray-100'>
-              Además de mi enfoque en el frontend, también tengo conocimiento en el backend.
-              Tengo conocimiento en lenguajes como 
-              <span className='text-teal-500'> Python</span>, 
-              <span className='text-teal-500'> Java</span> y
-              <span className='text-teal-500'> C#</span>.
-              También estoy familiarizado con bases de datos como <span className='text-teal-500'> PostgreSQL</span>.
-            </p>
-          </div>
-         
-        </section>
-        <section>
-          <div className='text-center'>
-          <h3 className='text-2xl sm:text-3xl py-1 md:py-4 mt-8 dark:text-gray-500'>Portafolio</h3>
+          <h3 className='text-2xl sm:text-3xl py-1 md:py-4 mt-8 dark:text-gray-500'>Proyectos</h3>
           <p className='text-sm sm:text-md py-2 leading-6 sm:leading-8 text-gray-800 md:text-xl max-w-3xl mx-auto dark:text-gray-100'> 
-            Durante mi aprendizaje he realizado distintos proyectos como la 
-            <span className='text-teal-500'> replicación de una imagen </span> solo con CSS, una
-            <span className='text-teal-500'> memoria </span>, una
-            <span className='text-teal-500'> calculadora </span> y una
-            <span className='text-teal-500'> réplica de la página de Bootstrap </span> utilizando React y 
-            finalmente un <span className='text-teal-500'> juego de batallas pokemon </span> con KaboomJS.            
+            Durante mi aprendizaje he realizado distintos proyectos            
           </p>
           </div>
           {/* Proyectos */}
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-10'>
-            <div className='flex flex-col items-center shadow-lg rounded-xl p-4 dark:bg-slate-700 dark:shadow-black'>
-              <Image src={web1} className='rounded-lg mb-4' width={'100%'} height={'100%'}/>
-              <a className='text-base sm:text-lg cursor-pointer bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md' 
-              href='http://107.21.144.181:3180/' >Ver más</a>
-            </div>
-            <div className='flex flex-col items-center shadow-lg rounded-xl p-4 dark:bg-slate-700 dark:shadow-black'>
-              <Image src={web3} className='rounded-lg mb-4' width={'100%'} height={'100%'}/>
-              <a className='text-base sm:text-lg cursor-pointer bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md' 
-              href='https://memoria-774c0.web.app/'>Ver más</a>
-            </div>
-            <div className='flex flex-col items-center shadow-lg rounded-xl p-4 dark:bg-slate-700 dark:shadow-black'>
-              <Image src={web4} className='rounded-lg mb-4'  width={'100%'} height={'100%'}/>
-              <a className='text-base sm:text-lg cursor-pointer bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md' 
-              href='https://calculadora-b5c89.web.app/'>Ver más</a>
-            </div>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-10'>            
             <div className='flex flex-col items-center shadow-lg rounded-xl p-4 dark:bg-slate-700 dark:shadow-black'>
               <Image src={web5} className='rounded-lg mb-4' width={'100%'} height={'100%'}/>
               <a className='text-base sm:text-lg cursor-pointer bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md' 
               href='https://bootstrap-fa53a.web.app/'>Ver más</a>
-            </div>
-            <div className='flex flex-col items-center shadow-lg rounded-xl p-4 dark:bg-slate-700 dark:shadow-black'>
-              <Image src={web6} className='rounded-lg mb-4' width={'100%'} height={'100%'}/>
-              <a className='text-base sm:text-lg cursor-pointer bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md' 
-              href='https://pokemonbattlegame-c7b24.web.app/'>Ver más</a>
             </div>
           </div>
         </section>
